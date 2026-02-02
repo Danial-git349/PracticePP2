b72f997dd177080dd3add8c100d2a99016290aed
Danial <dujsenbajdanial10@gmail.com>
2026-02-03 00:46:50 +0500

    add if else

---
+++ if_else/if_else.py
@@ -0,0 +1,47 @@
+# Example 1
+if 3 > 5:
+    print("Yes")
+else:
+    print("No")
+
+# Example 2
+x = 4
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Example 3
+age = 15
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+# Example 4
+password = "123"
+if password == "123":
+    print("Correct")
+else:
+    print("Wrong")
+
+# Example 5
+if False:
+    print("True")
+else:
+    print("False")
+
+# Example 6
+number = 0
+if number > 0:
+    print("Positive")
+else:
+    print("Zero or negative")
+
+# Example 7
+logged_in = False
+if logged_in:
+    print("Welcome")
+else:
+    print("Please log in")
+
---
+++ if_else/if_statement.py
@@ -0,0 +1,32 @@
+# Example 1
+if 5 > 3:
+    print("Yes")
+
+# Example 2
+if 10 == 10:
+    print("Equal")
+
+# Example 3
+x = 4
+if x > 0:
+    print("Positive")
+
+# Example 4
+age = 18
+if age >= 18:
+    print("Adult")
+
+# Example 5
+if True:
+    print("Always runs")
+# Example 6
+x = 100
+if x == 100:
+    print("Hundred")
+
+# Example 7
+flag = True
+if flag:
+    print("Flag is true")
+
+
