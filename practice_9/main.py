48847ae9704544e57ae0d6b1dec3025fd2fbbcb4
Danial <dujsenbajdanial10@gmail.com>
2026-04-14 10:40:13 +0500

    dfg

--- practice_9/main.py
+++ practice_9/main.py
@@ -10,13 +10,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mickey Clock")
 
 # Load image
-hand_img = pygame.image.load("images/Mickey_hand.png").convert_alpha()
+def trim_image(image):
+    rect = image.get_bounding_rect()
+    cropped = image.subsurface(rect).copy()
+    return cropped
+sec_hand = pygame.image.load("practice_9/images/Mickey_handleft.png").convert_alpha()
+min_hand = pygame.image.load("practice_9/images/Mickey_hand.png").convert_alpha()
 
 # OPTIONAL: fix rotation if needed
 # hand_img = pygame.transform.rotate(hand_img, 90)
 
 # Resize (optional)
-hand_img = pygame.transform.scale(hand_img, (200, 200))
+sec_hand = pygame.transform.scale(sec_hand, (120, 120))
+min_hand = pygame.transform.scale(min_hand, (120, 120))
 
 center = (WIDTH // 2, HEIGHT // 2)
 
@@ -29,7 +35,7 @@ def draw_hand(image, angle, position):
 
 running = True
 while running:
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,8 +45,8 @@ while running:
     sec_angle, min_angle = get_angles()
 
     # 🖐 Draw hands
-    draw_hand(hand_img, sec_angle, center)   # seconds
-    draw_hand(hand_img, min_angle, center)   # minutes
+    draw_hand(sec_hand, sec_angle, center)   # left hand = seconds
+    draw_hand(min_hand, min_angle, center)   # right hand = minutes   
 
     pygame.display.flip()
     clock.tick(1)  # update every second
